069f12a58513e809eb3cd6f12b56f5208753d7ed
jaekangchoi <jkchoi7091@knou.ac.kr>
2023-06-05 23:48:32 +0900

    deploy.sh는 이전에 step1에서 했던 deploy.sh가 아니라 현재 폴더에 맞춰서 새로 만들어 주었다

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,27 +3,18 @@
 REPOSITORY=/home/ec2-user/app2/step2
 PROJECT_NAME=board #해당 위치에 properties에 작성한 프로젝트명과 동일하게 작성
 echo "> Build 파일 복사"
-cd $REPOSITORY/$PROJECT_NAME/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo ">Git Pull"
-git pull
 
-echo ">프로젝트 빌드 시작"
-./gradlew build
+echo "> Build 파일 복사"
 
-echo ">stpe2 디렉토리로 이동"
-
-cd $REPOSITORY
-
-echo ">빌드 파일 복사"
-
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
 
-echo  "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -33,11 +24,19 @@ else
     sleep 5
 fi
 
+
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/| grep jar |tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
 
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app2/application-real.properties \
