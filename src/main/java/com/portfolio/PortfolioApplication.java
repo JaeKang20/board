e5daedde2f800aa2f40b1c6f1f414a59aa48c4ba
jaekangchoi <jkchoi7091@knou.ac.kr>
2023-06-05 23:05:11 +0900

    CI를 위한 변경. deploy.sh 파일과
    Applicain.java,application.properties파일, appspec.yml파일 수정

--- src/main/java/com/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/PortfolioApplication.java
@@ -2,10 +2,13 @@ package com.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class PortfolioApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
+		SpringApplication application = new SpringApplication(PortfolioApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 }
