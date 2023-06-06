bf34a1c19b5e7101a80f4afa57807498ba547284
jaekangchoi <jkchoi7091@knou.ac.kr>
2023-06-06 23:13:38 +0900

    Repository와 prooject name, nohup.out 출력 부분, appspec.yml 에서 실행해야하는 deploy.sh 부분을 변경해주었다.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app2/step2/zip
+REPOSITORY=/home/ec2-user/app2/step2/
 PROJECT_NAME=inflearn #해당 위치에 properties에 작성한 프로젝트명과 동일하게 작성 *jar파일로 생성되는 인텔리제이 프로젝트명과 동일해야함.
 
-
+echo "> 경로 이동"
+cd $REPOSITORY
 echo "> Build 파일 복사"
-cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $PROJECT_NAME)
@@ -24,8 +25,7 @@ echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/| grep jar |tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
-echo "> 해당 경로로 이동"
-cd $REPOSITORY
+
 
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
@@ -34,4 +34,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
         -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app2/application-real.properties \
         -Dspring.profiles.active=real \
-    $REPOSITORY/$JAR_NAME  2>&1 &
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
